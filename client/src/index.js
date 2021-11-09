f41e583e5283ca71c81b3ad6df877bcc80d3761c
Real Fake <mrfrostaman@gmail.com>
2021-11-09 13:26:31 +0200

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import MontyHall from "./components/MontyHall";
 import reportWebVitals from './reportWebVitals';
 
