3e3d5412c7cebfefec9a5996891c9d1762a6c973
YuraA30 <61061567+Yusk03@users.noreply.github.com>
2021-11-09 18:22:51 +0200

    mongoose fixes

--- server/controllers/count-controller.js
+++ server/controllers/count-controller.js
@@ -3,19 +3,27 @@ const Result = require('../models/result-model')
 class CountController {
   async increment(req, res, next) {
     try {
-      const {unchanged, changed} = req.body;
-      const stat = await Result.find({}, {unchanged: 1, changed: 1, _id: 1}).lean();
-      if (unchanged) {
-        const newUnchanged = stat.unchanged + 1;
-        await Result.updateOne({_id: stat._id}, {unchanged: newUnchanged});
-        return res.json({status: 'ACCESS'});
-      } else if (changed) {
-        const newChanged = stat.changed + 1;
-        await Result.updateOne({_id: stat._id}, {changed: newChanged});
-        return res.json({status: 'ACCESS'});
+      const {status} = req.body;
+      // const stat = await Result.find({}, {unchanged: 1, changed: 1, _id: 1}).lean();
+      // return res.json(stat);
+      const count = await Result.countDocuments();
+      if (count) {
+        const stat = await Result.find({}, {unchanged: 1, changed: 1, _id: 1}).lean();
+        if (status) {
+          const newUnchanged = stat[0].changed + 1;
+          await Result.updateOne({_id: stat[0]._id}, {unchanged: newUnchanged});
+        } else {
+          const newChanged = stat[0].changed + 1;
+          await Result.updateOne({_id: stat[0]._id}, {changed: newChanged});
+        }
       } else {
-        return res.json({status: 'UNKNOWN ERROR'});
+        if (status) {
+          await Result.create({unchanged: 0, changed: 1});
+        } else {
+          await Result.create({unchanged: 1, changed: 0});
+        }
       }
+      return res.json({status: 'ACCESS'});
     } catch (e) {
       console.log(e);
     }
@@ -23,7 +31,7 @@ class CountController {
 
   async statistic(req, res, next) {
     const stat = await Result.find({}, {unchanged: 1, changed: 1, _id: 0}).lean();
-    return res.json(stat);
+    return res.json({changed: stat[0].changed, unchanged: stat[0].unchanged});
   }
 }
 
